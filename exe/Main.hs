51387407d344bbbc9e99fcb783c136728d66cf53
0x_ontos <git.ontos@tomas-novotny.slmail.me>
2025-08-28 16:48:25 +0200

    advancing semi-haskell code

--- exe/Main.hs
+++ exe/Main.hs
@@ -15,10 +15,10 @@ import qualified Data.Text as T
 --------- INTERNAL
 
 
-let defaultConfigPath = "$XDG_CONFIG_HOME/lono/config"
-let alternativeConfigPath = "$HOME/.config/lono/config"
-let tmpPath = "/tmp/.temp_note_of_lono"
-let defaultDBPath = "$HOME/.local/lono/db.json"
+--let defaultConfigPath = "$XDG_CONFIG_HOME/lono/config"
+--let alternativeConfigPath = "$HOME/.config/lono/config"
+--let tmpPath = "/tmp/.temp_note_of_lono"
+--let defaultDBPath = "$HOME/.local/lono/db.json"
 
 type NotePath = FilePath -- FilePath is String
 type EDITOR = String
@@ -45,8 +45,37 @@ instance FromJSON NoteValue where
 
 -- figureOutPath :: String -> IO (String) -- for example if user do /folder1/folder2/.../note or just note (so adding current path) or ./folderx/note (so we add current path to it from left)
 
-getConfigPath :: CONFIG_PATH
-getConfigPath = if doesPathExist defaultConfigPath then defaultConfigPath else alternativeConfigPath
+
+defaultConfigPath :: IO FilePath
+defaultConfigPath = do
+  xdgConfig <- lookupEnv "XDG_CONFIG_HOME"
+  case xdgConfig of
+    Just path -> return (path </> "lono" </> "config")
+    Nothing -> do
+      home <- lookupEnv "HOME"
+      case home of
+        Just path -> return (path </> ".config" </> "lono" </> "config")
+        Nothing -> error "HOME environment variable not set"
+
+
+alternativeConfigPath :: IO FilePath
+alternativeConfigPath = do
+  home <- lookupEnv "HOME"
+  case home of
+    Just path -> return (path </> ".lono" </> "config")
+    Nothing -> error "HOME environment variable not set"
+
+
+tmpPath :: FilePath
+tmpPath = "/tmp/.temp_note_of_lono"
+
+
+defaultDBPath :: IO FilePath
+defaultDBPath = do
+  home <- lookupEnv "HOME"
+  case home of
+    Just path -> return (path </> ".local" </> "lono" </> "db.json")
+    Nothing -> error "HOME environment variable not set"
 
 
 createConfigurationFile :: IO ()
@@ -78,25 +107,31 @@ updateConfigValue configPath key newValue = writeFile configPath (unlines $ map 
 ---------- DATABASE
 
 
-createDB :: FilePath -> IO (NoteDB) -- filePath is DB_PATH
-createDB path = b.writeFile path (encodePretty NoteDb)
+createDB :: FilePath -> IO ()
+createDB path = B.writeFile path (encodePretty $ Directory HM.empty)
 
 
-loadDB :: FilePath -> IO (NoteDB)
+loadDB :: FilePath -> IO NoteValue
+loadDB path = case eitherDecode (B.readFile path) of
+    Left err -> error $ "Failed to parse DB: " ++ err
+    Right db -> return db
 
 
-updateDB :: NoteDB -> FilePath -> IO ()
+updateDB :: NoteValue -> FilePath -> IO ()
+updateDB db path = B.writeFile path (encodePretty db)
 
 
-writeTmpNoteAsFile :: NoteDB -> IO ()
-writeTmpNoteAsFile db = B.writeFile tmpPath (encodePretty db)
+-- tmpPath is global variable
+writeTmpNoteAsFile :: Text -> IO ()
+writeTmpNoteAsFile content = TIO.writeFile tmpPath content
 
 
--- updateTmpNoteFileIntoDB
+readTmpNoteFile :: IO Text
+readTmpNoteFile = TIO.readFile tmpPath
 
 
--- deleteTmpNoteFile
-
+deleteTmpNoteFile :: IO ()
+deleteTmpNoteFile = removeFile tmpPath `catch` (\_ -> return ()) -- ignore if file doesn't exist
 
 
 
@@ -107,60 +142,87 @@ writeTmpNoteAsFile db = B.writeFile tmpPath (encodePretty db)
 --------- APP INTERFACE
 
 ls :: ARGS -> IO () -- ls -all
-ls [] = -- will print all notes in current path (listDirectory) 
-ls "./" = -- will print all known notes in current and sub-folders
-ls "/" = -- will print all notes paths
-ls x = raise Error 
+ls [] = putStrLn "~"-- will print all notes in current path (listDirectory)
+ls "./" = putStrLn "~"--- will print all known notes in current and sub-folders
+ls "/" = putStrLn "~"-- will print all notes paths
+ls x = error "error"
+
 
 mv :: ARGS -> IO ()
-mv (selectedNote:newNotePath:[]) = -- need to have correct paths (figure out paths)
-mv x = raise Error
+mv (selectedNote:newNotePath:[]) = -putStrLn "~"-- need to have correct paths (figure out paths)
+mv x = error "error"
+
 
 rm :: ARGS -> IO ()
 rm [] = putStrLn "No name/path of note was provided."
-rm (note_path:[]) = note_path
-rm x = raiseError
+rm (note_path:[]) = -putStrLn "~"--note_path
+rm x = error "error"
+
 
 chosenArg :: FilePath -> IO () -- so basically we should only get one arg which should be name of note (path). Make sure the path is correct. Load DB from json file, create tmp file, cmd terminal editor, update DB. Update DB file (json).
-chosenArg
-
-setupArg :: String -> IO () 
-setupArg cli_args =
- | len == 1 = if (head cli_args) == "--create-db" then 
- | len == 2 = f (head cli_args) (last cli_args)
- | otherwise = _
- where
-     len = length cli_args
-     f "--set-editor" p = updateConfigValue "EDITOR" p
-     f "--set-db" p = if doesPathExist p then updateConfigValue "DB_PATH" p else raise Error
-     f _ _ = _
-
-
-main :: IO ()
-main = do 
-    config_path <- getConfigPath
-    when (config_path == "") $ createConfigurationFile
-
-    args <- getArgs
-    setupArg args
-
-    (editor, db_path) <- loadConfigurationFile config_path
-    when (editor == "") $ putStrLn "Editor is not set. Run: `lono --set-editor <editor of your choosing>"
-    when (db_path == "") $ putStrLn "Database of notes is not set. Either create it with `lono --create-db` or set existing one with `lono --set-db <path to db.json>`."
-
-    when (doesFileExist db_path) $ raise Error
-
-    --when (length args == 0) $ startTerminalGUI
-    --when (length args == 1) $ singleArg (head args)
-    first_arg <- head args
-    when ("ls" == first_arg) $ ls args
-    when ("mv" == first_arg) $ mv args -- basically rename
-    when ("rm" == first_arg) $ rm args
-
-    when (length args == 1) $ chosenNote (args !! 1) 
+chosenArg notePath editor dbPath = do
+  putStrLn $ "Editing: " ++ notePath
+  -- 1. Load the entire database
+  -- currentDB <- loadDB dbPath
+  -- 2. Look up the current content of 'notePath' in the DB
+  -- 3. Write that content to tmpPath (or empty string if new file)
+  -- 4. Run the editor: System.Process.callCommand (editor ++ " " ++ tmpPath)
+  -- 5. Read the content back from tmpPath
+  -- 6. Update the DB HashMap with the new content at the key (T.pack notePath)
+  -- 7. Save the DB back to file
+  -- 8. Delete the tmp file
+  putStrLn "chosenArg not yet implemented"
 
 
+setupArg :: [String] -> IO ()
+setupArg args = case args of
+  ["--create-db"] -> do
+    dbPath <- defaultDBPath
+    createDB dbPath
+    putStrLn $ "Created new database at: " ++ dbPath
+  ["--set-editor", editorPath] -> do
+    configPath <- getConfigPath
+    updateConfigValue configPath "EDITOR" editorPath
+  ["--set-db", dbPath] -> do
+    exists <- doesPathExist dbPath
+    if exists
+      then do
+        configPath <- getConfigPath
+        updateConfigValue configPath "DB_PATH" dbPath
+      else error "Database path does not exist."
+  _ -> putStrLn "Unknown setup command."
 
     
+main :: IO ()
+main = do
+  configPath <- getConfigPath
+  configExists <- doesFileExist configPath
+  when (not configExists) $ createConfigurationFile
 
+  args <- getArgs
+
+  -- Check for setup commands first
+  case args of
+    ("--create-db":_) -> setupArg args >> return ()
+    ("--set-editor":_) -> setupArg args >> return ()
+    ("--set-db":_) -> setupArg args >> return ()
+    _ -> return () -- Continue with normal app flow
+
+  -- Only load config and run the app if it wasn't a setup command
+  (editor, db_path) <- loadConfigurationFile configPath
+
+  when (null editor) $ putStrLn "Editor is not set. Run: `lono --set-editor <editor>`"
+  when (null db_path) $ putStrLn "Database path is not set. Run: `lono --set-db <path>`"
+
+  dbExists <- doesFileExist db_path
+  when (not dbExists) $ error "Database file does not exist. Create it with `lono --create-db`."
+
+  case args of
+    [] -> putStrLn "Terminal GUI not implemented yet. Please provide a command." -- startTerminalGUI
+    ["ls"] -> ls []
+    ("ls":rest) -> ls rest
+    ("mv":rest) -> mv rest
+    ("rm":rest) -> rm rest
+    [noteName] -> chosenArg noteName editor db_path
+    _ -> error "Invalid command or too many arguments."
 
